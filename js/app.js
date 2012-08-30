c91527f8ea170f950fdb3ad311c3b84c01f6ab39
aji <a.ji@daum.net>
2012-08-30 11:18:55 +0900

    설정추가

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 $(function() {
 
-    var BV = new $.BigVideo(),
+
+    var cp = $.cookie('cp') || 'all',
+        BV = new $.BigVideo(),
         newsList = {},
-        apiUrl = 'http://media.daum.net/api/service/tv/category/all.jsonp?countPerPage=300&callback=?',
+        apiUrl = 'http://media.daum.net/api/service/tv/'+ ((cp==="all") ? 'category/all' : cp ) +'.jsonp?countPerPage=100&callback=?',
         vodUrl = 'http://rt.flvs.daum.net:8080/RTES/Redirect?vid=',
         $listbox = $('#listbox');
 
+console.log(apiUrl)
 
     function random (min, max) {
         return Math.floor(Math.random() * (max - min + 1) + min);
@@ -34,7 +37,8 @@ $(function() {
 
 
         BV.show(vodUrl+vid);
-        $('#title').html(news.title);
+        // console.log(news)
+        $('#title').html(news.title + ' <small>'+ news.cpKorName  +' '+ news.regDate +' '+  news.regTime + '</small>');
     }
     function buildList(){
         var length = newsList.length,
@@ -76,6 +80,7 @@ $(function() {
     BV.init();
     loadData();
 
+    $('#cp-selector ').val(cp);
 
     //tooltip
     // $('.btnbox a').tooltip({placement:'botton'});
@@ -85,7 +90,7 @@ $(function() {
 
 
     //event
-    $('#btn-refresh').click(function(){
+    $('#btn-random').click(function(){
         showBigNews();
     });
 
@@ -96,4 +101,88 @@ $(function() {
     $listbox.click(function(){
         $listbox.hide();
     });
+
+    $('#btn-configsave').click(function(){
+
+        $.cookie('cp',  $('#cp-selector ').val());
+        location.reload();
+        // $('#configbox').modal('hide');
+    });
 });
+
+
+
+
+
+/*jshint eqnull:true */
+/*!
+ * jQuery Cookie Plugin v1.2
+ * https://github.com/carhartl/jquery-cookie
+ *
+ * Copyright 2011, Klaus Hartl
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.opensource.org/licenses/GPL-2.0
+ */
+(function ($, document, undefined) {
+
+    var pluses = /\+/g;
+
+    function raw(s) {
+        return s;
+    }
+
+    function decoded(s) {
+        return decodeURIComponent(s.replace(pluses, ' '));
+    }
+
+    var config = $.cookie = function (key, value, options) {
+
+        // write
+        if (value !== undefined) {
+            options = $.extend({}, config.defaults, options);
+
+            if (value === null) {
+                options.expires = -1;
+            }
+
+            if (typeof options.expires === 'number') {
+                var days = options.expires, t = options.expires = new Date();
+                t.setDate(t.getDate() + days);
+            }
+
+            value = config.json ? JSON.stringify(value) : String(value);
+
+            return (document.cookie = [
+                encodeURIComponent(key), '=', config.raw ? value : encodeURIComponent(value),
+                options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
+                options.path    ? '; path=' + options.path : '',
+                options.domain  ? '; domain=' + options.domain : '',
+                options.secure  ? '; secure' : ''
+            ].join(''));
+        }
+
+        // read
+        var decode = config.raw ? raw : decoded;
+        var cookies = document.cookie.split('; ');
+        for (var i = 0, parts; (parts = cookies[i] && cookies[i].split('=')); i++) {
+            if (decode(parts.shift()) === key) {
+                var cookie = decode(parts.join('='));
+                return config.json ? JSON.parse(cookie) : cookie;
+            }
+        }
+
+        return null;
+    };
+
+    config.defaults = {};
+
+    $.removeCookie = function (key, options) {
+        if ($.cookie(key, options) !== null) {
+            $.cookie(key, null, options);
+            return true;
+        }
+        return false;
+    };
+
+})(jQuery, document);
